2e8ff2461ff9f4af4be8c11b3fb04514db77b21c
cyberhacker444 <cyberhacker4406@gmail.com>
2026-04-18 14:12:33 +0200

    Add service worker for caching assets
    Implement service worker for caching and fetch handling.

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "schoolar-pro-v3";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js",
+        "/icon-192.png",
+        "/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
